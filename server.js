59345842f6b777c19122be135e8b96807708cf3a
jasoncala <jasonfcala@gmail.com>
2021-10-03 19:55:00 -0400

    more front end work

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     const code = `Welcome to TextShare!
@@ -8,6 +9,10 @@ app.get('/', (req, res) => {
 Use the commands in the top right corner 
 to create a new file to share with others.`
     res.render('code-display', { code })
+})
+
+app.get('/new', (req, res) => {
+    res.render('new')
 })
 
 app.listen(3000)
